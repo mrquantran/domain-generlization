ac2a45fe833f77877adb58564b6d251c810bd61f
Beginner <quantran2381@gmail.com>
2024-10-28 17:06:28 +0200

    feat: get source and dataset

--- domainbed/cyclegan/utils.py
+++ domainbed/cyclegan/utils.py
@@ -1,13 +1,14 @@
 def get_sources(dataset: str, targets: list):
-    if dataset == 'PACS':
-        # ENVIRONMENTS = ["A", "C", "P", "S"]
+    if dataset == "PACS":
         domains = ["art_painting", "cartoon", "photo", "sketch"]
-        domain_names = {
-            0: "art_painting",
-            1: "cartoon",
-            2: "photo",
-            3: "sketch"
-        }
+        domain_names = {0: "art_painting", 1: "cartoon", 2: "photo", 3: "sketch"}
+        for i in targets:
+            domains.remove(domain_names[i])
+        return domains
+
+    elif dataset == "OfficeHome":
+        domains = ["Art", "Clipart", "Product", "Real_World"]
+        domain_names = {0: "Art", 1: "Clipart", 2: "Product", 3: "Real_World"}
         for i in targets:
             domains.remove(domain_names[i])
         return domains
